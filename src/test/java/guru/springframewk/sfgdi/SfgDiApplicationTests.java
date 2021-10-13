065de726e3c04b28be201b860efd601272f5f6a2
Itamar Carvalho <itamarc@gmail.com>
2021-10-13 18:53:33 -0300

    First dependency injection example
    Up to lesson 32

---
+++ src/test/java/guru/springframewk/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.guru.springframewk.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
